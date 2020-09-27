4e18e98c974de495ff4368f016a5e57e777c2b6b
stamic1 <bhatgaurav96@gmail.com>
2020-09-27 12:07:49 +0530

    clean Up

---
+++ tinderclone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAEResQapvEl7a4-x58KTLF0mE1iCxBbdw",
+  authDomain: "tinder-app-clone-7bf9d.firebaseapp.com",
+  databaseURL: "https://tinder-app-clone-7bf9d.firebaseio.com",
+  projectId: "tinder-app-clone-7bf9d",
+  storageBucket: "tinder-app-clone-7bf9d.appspot.com",
+  messagingSenderId: "913032979965",
+  appId: "1:913032979965:web:4ae031bace18163703e594",
+  measurementId: "G-N6KTP2TLC8",
+};
