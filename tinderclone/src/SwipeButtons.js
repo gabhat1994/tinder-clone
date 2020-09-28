a1cd5da494474061156e469aaebfbdf01cd7e39c
stamic1 <bhatgaurav96@gmail.com>
2020-09-28 11:03:15 +0530

    Swipe Buttons done

--- tinderclone/src/SwipeButtons.js
+++ tinderclone/src/SwipeButtons.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./SwipeButtons.css";
+import ReplayIcon from "@material-ui/icons/Replay";
+import CloseIcon from "@material-ui/icons/Close";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import IconButton from "@material-ui/core/IconButton";
 
 export default function SwipeButtons() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="swipebuttons">
+      <IconButton className="swipebuttons__repeat">
+        <ReplayIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipebuttons__left">
+        <CloseIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipebuttons__star">
+        <StarRateIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipebuttons__right">
+        <FavoriteIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipebuttons__lightening">
+        <FlashOnIcon fontSize="large" />
+      </IconButton>
+    </div>
+  );
 }
